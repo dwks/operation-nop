e5347012c4c00cdf4db51598bb7c64eefa27adf9
dwk <dwks42@gmail.com>
2014-02-28 20:07:48 -0800

    First scraping script that grabs average ozone levels per year.

---
+++ scrape/airquality.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+my $url = 'http://www.ec.gc.ca/indicateurs-indicators/25C196D8-5835-4A86-BCD0-7731C7109610/AirQuality_O3_Nat_EN.csv';
+my @data = `curl --silent "$url"`;
+
+for(my $count = 0; $count < @data; $count ++) {
+    my $line = $data[$count];
+    $line =~ s/\s+$//;
+
+    next if($count < 3);  # headers
+
+    my ($year, $average, $peak) = split /,/, $line;
+
+    last if($year eq '');  # end of data
+    print "$year $average $peak\n";
+}
