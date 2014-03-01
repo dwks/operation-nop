b7af843c3e9356656514e58ee3fe79ca076700f7
Michael Carius <michael.a.carius@gmail.com>
2014-03-01 10:36:48 -0800

    Create HTTP POST request class for client.

--- client/App/src/ca/ubc/cs/nop/MainActivity.java
+++ client/App/src/ca/ubc/cs/nop/MainActivity.java
@@ -3,6 +3,9 @@ package ca.ubc.cs.nop;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.widget.TextView;
+import android.util.Log;
+
 public class MainActivity extends Activity
 {
     /** Called when the activity is first created. */
@@ -11,5 +14,22 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final TextView tv = (TextView) findViewById(R.id.text);
+
+        new RequestTask("http://sirius.nss.cs.ubc.ca:8080/find_clinic", new RequestHandler() {
+            public void onSuccess(String response) {
+                tv.setText(response);
+            }
+
+            public void onFailure() {
+                tv.setText("Error");
+            }
+        })
+            .bind("pos_x", "0")
+            .bind("pos_y", "0")
+            .bind("min_results", "1")
+            .bind("max_results", "1")
+            .execute();
     }
 }
---
+++ client/App/src/ca/ubc/cs/nop/RequestTask.java
@@ -0,0 +1,103 @@
+package ca.ubc.cs.nop;
+
+// java stuff
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+// http stuff
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+
+import org.apache.http.client.ClientProtocolException;
+
+// android stuff
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class RequestTask extends AsyncTask<Void, Void, Boolean> {
+    // parameters
+    private String url;
+    private RequestHandler handler;
+
+    // internal state
+    private ArrayList<NameValuePair> parameters = new ArrayList<NameValuePair>();
+    private String response;
+
+    public RequestTask(String url, RequestHandler handler) {
+        this.url = url;
+        this.handler = handler;
+    }
+
+    public RequestTask bind(String name, String value) {
+        parameters.add(new BasicNameValuePair(name, value));
+        return this;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... args) {
+        Log.v("RequestTask", "Begin background execution");
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost request = new HttpPost(url);
+
+        try {
+            Log.v("RequestTask", "Begin request");
+
+            request.setEntity(new UrlEncodedFormEntity(parameters));
+            Log.v("RequestTask", "Request successfully encoded");
+
+            HttpResponse response = client.execute(request);
+            Log.v("RequestTask", "Request completed");
+
+            StatusLine statusLine = response.getStatusLine();
+
+            if(statusLine.getStatusCode() != HttpStatus.SC_OK) {
+                Log.e("RequestTask", "Request completed with errors: " + statusLine.getReasonPhrase());
+                return false;
+            }
+
+            Log.v("RequestTask", "Request completed successfully");
+            ByteArrayOutputStream responseBody = new ByteArrayOutputStream();
+            responseBody.writeTo(responseBody);
+            this.response = responseBody.toString();
+
+            return true;
+        }
+
+        catch(UnsupportedEncodingException e) {
+            Log.e("RequestTask", "UnsupportedEncodingException: " + e.getMessage());
+            return false;
+        }
+
+        catch(ClientProtocolException e) {
+            Log.e("RequestTask", "ClientProtocolException: " + e.getMessage());
+            return false;
+        }
+
+        catch(IOException e) {
+            Log.e("RequestTask", "IOException: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean ok) {
+        if(ok)
+            handler.onSuccess(response);
+
+        else
+            handler.onFailure();
+    }
+}
