32e3a73ddf3a2542bc9f8be7e3f56c2fc8674599
Michael Carius <michael.a.carius@gmail.com>
2014-03-01 16:22:18 -0800

    Move map into separate activity.

--- client/App/src/ca/ubc/cs/nop/MainActivity.java
+++ client/App/src/ca/ubc/cs/nop/MainActivity.java
@@ -1,27 +1,121 @@
 package ca.ubc.cs.nop;
 
-// generated, do not modify
-import android.app.Activity;
-import android.os.Bundle;
+// json decoder
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-// google play stuff
+// google play services stuff
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+
+// location services stuff
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import android.location.Location;
+
+// google maps stuff
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLng;
 
 // android stuff
+import android.app.Activity;
+import android.os.Bundle;
 import android.app.Dialog;
-import android.widget.TextView;
+import android.view.View;
 import android.util.Log;
 
-public class MainActivity extends Activity
+public class MainActivity
+    extends Activity
+    implements GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener
 {
-    /** Called when the activity is first created. */
+    private GoogleMap gmap;
+    private LocationClient locationClient;
+    private boolean locationServicesConnected = false;
+
+    // location service callbacks
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        // generated, do not modify
+    public void onConnected(Bundle dataBundle) {
+        Log.v("MainActivity", "Connected to location service");
+        locationServicesConnected = true;
+
+        // set up location updates
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(5000);
+        locationClient.requestLocationUpdates(locationRequest, this);
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.v("MainActivity", "Disconnected from location service");
+        locationServicesConnected = false;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.v("MainActivity", "Failed to connect to location service");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v("MainActivity", "Last location: " + Location.convert(location.getLongitude(), Location.FORMAT_DEGREES) + "/" + Location.convert(location.getLatitude(), Location.FORMAT_DEGREES) + " (accurate to within " + location.getAccuracy() + "m)");
+
+        new RequestTask("http://sirius.nss.cs.ubc.ca:8080/find_clinic", new RequestHandler() {
+            public void onSuccess(String response) {
+                Log.v("MainActivity", "Successfully retrieved clinic JSON");
+
+                try {
+                    JSONArray array = new JSONArray(response);
+
+                    // no data received
+                    if(array.length() == 0) {
+                        Log.v("MainActivity", "No clinics nearby");
+                        return;
+                    }
+
+                    JSONObject pos = array.getJSONObject(0);
+                    double x = pos.getDouble("POS_X");
+                    double y = pos.getDouble("POS_Y");
+
+                    gmap.addMarker(new MarkerOptions()
+                        .position(new LatLng(x, y))
+                        .title("Clinic Location"));
+                }
+
+                catch(JSONException e) {
+                    Log.e("MainActivity", "Invalid JSON returned: " + response);
+                }
+            }
+
+            public void onFailure() {
+                Log.v("MainActivity", "Failed to retrieve clinic JSON");
+            }
+        })
+            .bind("pos_x", Location.convert(location.getLongitude(), Location.FORMAT_DEGREES))
+            .bind("pos_y", Location.convert(location.getLatitude(), Location.FORMAT_DEGREES))
+            .bind("min_results", "1")
+            .bind("max_results", "1")
+            .execute();
+    }
+
+    // lifecycle callbacks
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    // button callbacks
+    public void showMapView(View button) {
+        setContentView(R.layout.map);
 
         // check for google play services (required by maps api)
         Log.v("MainActivity", "Checking for Google Play Services");
@@ -38,24 +132,16 @@ public class MainActivity extends Activity
         else
             Log.v("MainActivity", "Google Play Services up-to-date");
 
-        /*
-        // example that retrieves json and puts it in the default textview
-        final TextView tv = (TextView) findViewById(R.id.text);
+        // retrieve map state and enable location layer
+        Log.v("MainActivity", "Retrieving map state");
+        gmap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-        new RequestTask("http://sirius.nss.cs.ubc.ca:8080/find_clinic", new RequestHandler() {
-            public void onSuccess(String response) {
-                tv.setText(response);
-            }
+        Log.v("MainActivity", "Enabling My Location layer for map view");
+        gmap.setMyLocationEnabled(true);
 
-            public void onFailure() {
-                tv.setText("Error");
-            }
-        })
-            .bind("pos_x", "0")
-            .bind("pos_y", "0")
-            .bind("min_results", "2")
-            .bind("max_results", "8")
-            .execute();
-        */
+        // create location client
+        Log.v("MainActivity", "Creating location client");
+        locationClient = new LocationClient(this, this, this);
+        locationClient.connect();
     }
 }
