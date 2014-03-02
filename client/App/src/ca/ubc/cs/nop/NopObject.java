a77baae9e9154d1f93b1451c6e7c1c4d2ac990c3
Mahdi <tayarani@ubuntu.ubuntu-domain>
2014-03-02 12:34:41 -0800

    sizes to birds

--- client/App/src/ca/ubc/cs/nop/NopObject.java
+++ client/App/src/ca/ubc/cs/nop/NopObject.java
@@ -9,40 +9,64 @@ import android.view.SurfaceView;
 public class NopObject {
   int x_;
   int y_;
-  int width_;
-  int height_;
   int dropOffset_;
   boolean alive_;
-  Bitmap img_;
   Rect rect_;
+  int tickCounter_;
+  int frame_;
   public NopObject(
-      int x, int y, int width, int height, int dropOffset, boolean alive,
-      Bitmap img) {
+      int x, int y, int dropOffset, boolean alive, int seed) {
     x_ = x;
     y_ = y;
-    width_ = width;
-    height_ = height;
     dropOffset_ = dropOffset;
     alive_ = alive;
-    img_ = img;
 
     rect_ = new Rect();
-    rect_.left = x - width / 2;
-    rect_.right = x + width / 2;
-    rect_.top = y - height / 2;
-    rect_.bottom = y + height / 2;
+    setSize(0, 0);
+
+    frame_ = 0;
+    tickCounter_ = seed;
   }
 
   public void setAlive(boolean alive) {
     alive_ = alive;
   }
 
-  public void draw(Canvas canvas, Paint paint) {
-    // rect_.left = x - canvas.getWidth() / 2;
-    // rect_.right = x - width / 2;
-    // rect_.top = y - height / 2;
-    // rect_.bottom = y + height / 2;
+  public void draw(Canvas canvas, Paint paint,
+                   Bitmap alive0, Bitmap alive1,
+                   Bitmap dead0, Bitmap dead1) {
+    if (alive_) {
+      if (frame_ == 0) {
+        canvas.drawBitmap(alive0, null, rect_, paint);
+      } else {
+        canvas.drawBitmap(alive1, null, rect_, paint);
+      }
+    } else {
+      if (frame_ == 0) {
+        canvas.drawBitmap(dead0, null, rect_, paint);
+      } else {
+        canvas.drawBitmap(dead1, null, rect_, paint);
+      }
+    }
 
-    canvas.drawBitmap(img_, null, rect_, paint);
+    tickCounter_++;
+    if (frame_ == 0) {
+      if (tickCounter_ > 40) {
+        frame_ = 1;
+        tickCounter_ = 0;
+      }
+    } else {
+      if (tickCounter_ > 5) {
+        frame_ = 0;
+        tickCounter_ = 0;
+      }
+    }
+  }
+
+  public void setSize(int width, int height) {
+    rect_.left = x_- width / 2;
+    rect_.right = x_ + width / 2;
+    rect_.top = y_ - height / 2;
+    rect_.bottom = y_ + height / 2;
   }
 }
