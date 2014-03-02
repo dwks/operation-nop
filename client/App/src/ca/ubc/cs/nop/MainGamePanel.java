06120deac6790e19d947086fc55f0bd85ce90294
Mahdi <tayarani@ubuntu.ubuntu-domain>
2014-03-02 12:40:36 -0800

    set pos

--- client/App/src/ca/ubc/cs/nop/MainGamePanel.java
+++ client/App/src/ca/ubc/cs/nop/MainGamePanel.java
@@ -85,13 +85,13 @@ public MainGamePanel(Context context) {
     birdImg[3] = BitmapFactory.decodeResource(
         getResources(), R.drawable.bird_sad2_full);
 
-    branch_ = new NopObject(100, 100, 1, true, 0);
+    branch_ = new NopObject(1, true, 0);
     birds_ = new NopObject[5];
-    birds_[0] = new NopObject(100, 100, 1, true, 0);
-    birds_[1] = new NopObject(110, 110, 1, true, 34);
-    birds_[2] = new NopObject(120, 120, 1, true, 23);
-    birds_[3] = new NopObject(130, 130, 1, true, 10);
-    birds_[4] = new NopObject(240, 140, 1, false, 20);
+    birds_[0] = new NopObject(1, true, 0);
+    birds_[1] = new NopObject(1, true, 34);
+    birds_[2] = new NopObject(1, true, 23);
+    birds_[3] = new NopObject(1, true, 10);
+    birds_[4] = new NopObject(1, false, 20);
 }
 
 @Override
@@ -120,13 +120,23 @@ protected void onDraw(Canvas canvas) {
     // canvas.drawBitmap(branchImg, 0, 5, paintBranchImg);
     // canvas.drawBitmap(birdImg[0], 1, 5, paintBranchImg);
 
-    branch_.setSize(100, 100);
+    branch_.setSize(100, 100, 100, 100);
     branch_.draw(canvas, paintBranchImg, branchImg, branchImg, branchImg, branchImg);
 
-    for (NopObject bird : birds_) {
-      bird.setSize(100, 100);
-      bird.draw(canvas, paintBranchImg, birdImg[0], birdImg[1], birdImg[2], birdImg[3]);
-    }
+    birds_[0].setSize(110, 110, 100, 100);
+    birds_[0].draw(canvas, paintBranchImg, birdImg[0], birdImg[1], birdImg[2], birdImg[3]);
+
+    birds_[1].setSize(110, 110, 100, 100);
+    birds_[1].draw(canvas, paintBranchImg, birdImg[0], birdImg[1], birdImg[2], birdImg[3]);
+
+    birds_[2].setSize(110, 110, 100, 100);
+    birds_[2].draw(canvas, paintBranchImg, birdImg[0], birdImg[1], birdImg[2], birdImg[3]);
+
+    birds_[3].setSize(110, 110, 100, 100);
+    birds_[3].draw(canvas, paintBranchImg, birdImg[0], birdImg[1], birdImg[2], birdImg[3]);
+
+    birds_[4].setSize(110, 110, 100, 100);
+    birds_[4].draw(canvas, paintBranchImg, birdImg[0], birdImg[1], birdImg[2], birdImg[3]);
 }
 
   public void stopThread() {
