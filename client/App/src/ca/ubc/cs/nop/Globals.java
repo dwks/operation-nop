d450bc54856b0bd1e859eb2b364e4dc53f2707c9
Michael Carius <michael.a.carius@gmail.com>
2014-03-01 20:36:10 -0800

    Add location service.

---
+++ client/App/src/ca/ubc/cs/nop/Globals.java
@@ -0,0 +1,5 @@
+package ca.ubc.cs.nop;
+
+public class Globals {
+    public static final String SERVER = "http://sirius.nss.cs.ubc.ca:8080";
+}
--- client/App/src/ca/ubc/cs/nop/MainActivity.java
+++ client/App/src/ca/ubc/cs/nop/MainActivity.java
@@ -1,126 +1,51 @@
 package ca.ubc.cs.nop;
 
-// json decoder
-import org.json.JSONObject;
-import org.json.JSONArray;
-import org.json.JSONException;
-
 // google play services stuff
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 
-// location services stuff
-import com.google.android.gms.location.LocationClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import android.location.Location;
-
-// google maps stuff
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.LatLng;
-
 // android stuff
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.app.Dialog;
 import android.view.View;
 import android.util.Log;
-import android.os.Handler;
+import android.location.Location;
+import android.widget.Toast;
 
-public class MainActivity
-    extends Activity
-    implements GooglePlayServicesClient.ConnectionCallbacks,
-        GooglePlayServicesClient.OnConnectionFailedListener,
-        LocationListener
-{
-    public static final String SERVER = "http://sirius.nss.cs.ubc.ca:8080";
+public class MainActivity extends Activity {
+    // location service connection
+    private LocationService locationService;
 
-    private GoogleMap gmap;
-    private LocationClient locationClient;
-    private boolean locationServicesConnected = false;
+    private ServiceConnection locationServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder binder) {
+            Log.v("MainActivity", "Connected to location service");
+            locationService = ((LocationService.Binder) binder).getService();
+        }
 
-    // location service callbacks
-    @Override
-    public void onConnected(Bundle dataBundle) {
-        Log.v("MainActivity", "Connected to location service");
-        locationServicesConnected = true;
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            Log.v("MainActivity", "Disconnected from location service");
+            locationService = null;
+        }
+    };
 
-        // set up location updates
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5000);
-        locationRequest.setFastestInterval(5000);
-        locationClient.requestLocationUpdates(locationRequest, this);
-    }
-
-    @Override
-    public void onDisconnected() {
-        Log.v("MainActivity", "Disconnected from location service");
-        locationServicesConnected = false;
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        Log.v("MainActivity", "Failed to connect to location service");
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.v("MainActivity", "Last location: " + Location.convert(location.getLongitude(), Location.FORMAT_DEGREES) + "/" + Location.convert(location.getLatitude(), Location.FORMAT_DEGREES) + " (accurate to within " + location.getAccuracy() + "m)");
-
-        new RequestTask("http://sirius.nss.cs.ubc.ca:8080/find_clinic", new RequestHandler() {
-            public void onSuccess(String response) {
-                Log.v("MainActivity", "Successfully retrieved clinic JSON");
-
-                try {
-                    JSONArray array = new JSONArray(response);
-
-                    // no data received
-                    if(array.length() == 0) {
-                        Log.v("MainActivity", "No clinics nearby");
-                        return;
-                    }
-
-                    JSONObject pos = array.getJSONObject(0);
-                    double x = pos.getDouble("POS_X");
-                    double y = pos.getDouble("POS_Y");
-
-                    gmap.addMarker(new MarkerOptions()
-                        .position(new LatLng(x, y))
-                        .title("Clinic Location"));
-                }
-
-                catch(JSONException e) {
-                    Log.e("MainActivity", "Invalid JSON returned: " + response);
-                }
-            }
-
-            public void onFailure() {
-                Log.v("MainActivity", "Failed to retrieve clinic JSON");
-            }
-        })
-            .bind("pos_x", Location.convert(location.getLongitude(), Location.FORMAT_DEGREES))
-            .bind("pos_y", Location.convert(location.getLatitude(), Location.FORMAT_DEGREES))
-            .bind("min_results", "1")
-            .bind("max_results", "1")
-            .execute();
-    }
-
-    // lifecycle callbacks
+    // activity lifecycle
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.v("MainActivity", "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
 
-    // button callbacks
-    public void showMapView(View button) {
-        setContentView(R.layout.map);
-
-        // check for google play services (required by maps api)
+        // check for google play services (required by location services and maps api)
         Log.v("MainActivity", "Checking for Google Play Services");
         int gplayStatus = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
         if(gplayStatus != ConnectionResult.SUCCESS) {
@@ -134,44 +59,33 @@ public class MainActivity
 
         else
             Log.v("MainActivity", "Google Play Services up-to-date");
-
-        // retrieve map state and enable location layer
-        Log.v("MainActivity", "Retrieving map state");
-        gmap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-
-        Log.v("MainActivity", "Enabling My Location layer for map view");
-        gmap.setMyLocationEnabled(true);
-
-        // create location client
-        Log.v("MainActivity", "Creating location client");
-        locationClient = new LocationClient(this, this, this);
-        locationClient.connect();
-
-        setupNotificationQuery();
     }
 
-    private Handler handler = new Handler();
-    private static final int NOTIFICATION_PERIOD = 5*1000;
+    @Override
+    public void onStart() {
+        Log.v("MainActivity", "onStart");
 
-    private void setupNotificationQuery() {
-        handler.removeCallbacks(requestNotifications);
-        handler.postDelayed(requestNotifications, NOTIFICATION_PERIOD);
-        Log.v("MainActivity", "set up notification timer");
+        super.onStart();
+        Intent intent = new Intent(this, LocationService.class);
+        bindService(intent, locationServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
-    private Runnable requestNotifications = new Runnable() {
-        public void run() {
-            new RequestTask(SERVER + "/status", new RequestHandler() {
-                public void onSuccess(String response) {
-                    Log.v("MainActivity", "Got status: " + response);
-                }
+    @Override
+    public void onStop() {
+        Log.v("MainActivity", "onStop");
 
-                public void onFailure() {
-                    Log.v("MainActivity", "Failed to retrieve status");
-                }
-            }).execute();
+        super.onStop();
+        unbindService(locationServiceConnection);
+    }
 
-            handler.postDelayed(this, NOTIFICATION_PERIOD);
+    // button callbacks
+    public void test(View view) {
+        if(!locationService.isAvailable()) {
+            Toast.makeText(getApplicationContext(), "Location services unavailable", Toast.LENGTH_SHORT).show();
+            return;
         }
-    };
+
+        Location location = locationService.getLocation();
+        Toast.makeText(getApplicationContext(), "Current location: " + Location.convert(location.getLongitude(), Location.FORMAT_DEGREES) + "/" + Location.convert(location.getLatitude(), Location.FORMAT_DEGREES), Toast.LENGTH_SHORT).show();
+    }
 }
