427ccb011b5fd645385051baed3d3980d7f6b7e8
Mahdi <tayarani@ubuntu.ubuntu-domain>
2014-03-02 11:39:14 -0800

    palce holders for nopobject

--- client/App/src/ca/ubc/cs/nop/NopObject.java
+++ client/App/src/ca/ubc/cs/nop/NopObject.java
@@ -1,3 +1,26 @@
 package ca.ubc.cs.nop;
 
-public class NopObject
+public class NopObject {
+  float x_;
+  float y_;
+  float width_;
+  float height_;
+  float dropOffset_;
+  boolean alive_;
+  public NopObject(
+      float x, float y, float width, float height, float dropOffset, boolean alive) {
+    x_ = x;
+    y_ = y;
+    width_ = width;
+    height_ = height;
+    dropOffset_ = dropOffset;
+    alive_ = alive;
+  }
+
+  public void setAlive(boolean alive) {
+    alive_ = alive;
+  }
+
+  public void draw() {
+  }
+}
